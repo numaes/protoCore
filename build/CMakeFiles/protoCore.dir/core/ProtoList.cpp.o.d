659a9b3bd3299f2b3d8db89f95795e39688013d6
Gustavo Marino <gamarino@numaes.com>
2026-02-08 10:35:08 -0300

    [SYNCH]

--- build/CMakeFiles/protoCore.dir/core/ProtoList.cpp.o.d
+++ build/CMakeFiles/protoCore.dir/core/ProtoList.cpp.o.d
@@ -237,17 +237,4 @@ CMakeFiles/protoCore.dir/core/ProtoList.cpp.o: \
  /usr/include/c++/13/bits/hashtable_policy.h \
  /usr/include/c++/13/bits/enable_special_members.h \
  /usr/include/c++/13/bits/node_handle.h \
- /usr/include/c++/13/bits/erase_if.h /usr/include/c++/13/array \
- /usr/include/c++/13/chrono /usr/include/c++/13/sstream \
- /usr/include/c++/13/bits/sstream.tcc \
- /usr/include/c++/13/bits/chrono_io.h /usr/include/c++/13/iomanip \
- /usr/include/c++/13/locale \
- /usr/include/c++/13/bits/locale_facets_nonio.h \
- /usr/include/x86_64-linux-gnu/c++/13/bits/time_members.h \
- /usr/include/x86_64-linux-gnu/c++/13/bits/messages_members.h \
- /usr/include/libintl.h /usr/include/c++/13/bits/codecvt.h \
- /usr/include/c++/13/bits/locale_facets_nonio.tcc \
- /usr/include/c++/13/bits/locale_conv.h \
- /usr/include/c++/13/bits/quoted_string.h /usr/include/c++/13/format \
- /usr/include/c++/13/charconv /usr/include/c++/13/optional \
- /usr/include/c++/13/span /usr/include/c++/13/variant
+ /usr/include/c++/13/bits/erase_if.h /usr/include/c++/13/array
